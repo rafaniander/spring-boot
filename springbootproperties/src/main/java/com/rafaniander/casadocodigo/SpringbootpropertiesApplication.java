2d3442f1de1d88199798581b9eb8e74510d0280b
Rafael Marques <rafaelmarques1992@gmail.com>
2018-06-06 14:02:11 -0300

    Criando uma aplicação simples

---
+++ springbootproperties/src/main/java/com/rafaniander/casadocodigo/SpringbootpropertiesApplication.java
@@ -0,0 +1,12 @@
+package com.rafaniander.casadocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootpropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootpropertiesApplication.class, args);
+	}
+}
