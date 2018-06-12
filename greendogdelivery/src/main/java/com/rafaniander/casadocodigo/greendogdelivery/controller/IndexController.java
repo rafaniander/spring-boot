61232cba4a635893eb97793d3de021051ae546a6
Rafael Marques <rafaelmarques1992@gmail.com>
2018-06-12 17:20:02 -0300

    Anotações necessárias para o spring cloud

--- greendogdelivery/src/main/java/com/rafaniander/casadocodigo/greendogdelivery/controller/IndexController.java
+++ greendogdelivery/src/main/java/com/rafaniander/casadocodigo/greendogdelivery/controller/IndexController.java
@@ -4,12 +4,15 @@ import com.rafaniander.casadocodigo.greendogdelivery.dto.MensagemDTO;
 import java.util.Properties;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
-
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@EnableAutoConfiguration
+@RefreshScope 
 @Controller
 @RequestMapping("/")
 public class IndexController {
