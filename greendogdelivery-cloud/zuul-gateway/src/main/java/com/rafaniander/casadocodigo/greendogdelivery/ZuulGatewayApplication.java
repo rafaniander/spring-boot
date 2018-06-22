3c900183429c207de1f6d24c623a20ac13c1bed4
Rafael Marques <rafaelmarques1992@gmail.com>
2018-06-22 16:26:33 -0300

    Adicionando nova anotação no Zuul

--- greendogdelivery-cloud/zuul-gateway/src/main/java/com/rafaniander/casadocodigo/greendogdelivery/ZuulGatewayApplication.java
+++ greendogdelivery-cloud/zuul-gateway/src/main/java/com/rafaniander/casadocodigo/greendogdelivery/ZuulGatewayApplication.java
@@ -2,9 +2,11 @@ package com.rafaniander.casadocodigo.greendogdelivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
+@EnableEurekaClient
 @SpringCloudApplication
 public class ZuulGatewayApplication {
 
